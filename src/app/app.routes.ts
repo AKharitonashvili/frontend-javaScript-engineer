1756cd3bc373f66b9fa4426cc0d635c9abe1da78
AKharitonashvili <sandroxari@gmail.com>
2024-08-07 17:32:53 +0400

    feature add eslint prettier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/calendar', pathMatch: 'full' },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./calendar/calendar.component').then(x => x.CalendarComponent),
+  },
+];
